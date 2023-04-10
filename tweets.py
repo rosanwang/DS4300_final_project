a3404fe85ae082f738af68392c9cbf5ddca29c0a
Rosan Wang <rosanwang@yahoo.com>
2023-04-09 23:55:03 -0400

    added more key words

--- tweets.py
+++ tweets.py
@@ -2,13 +2,13 @@ import twint
 import datetime
 
 # for some reason only tweets from 2007-03-01 onwards exist
-for year in range(2007, 2022):
+for year in range(2008, 2023):
     for month in range(1, 12):
         since = datetime.datetime(year, month, 1)
         until = datetime.datetime(year, month + 1, 1)
 
         c = twint.Config()
-        c.Search = "economy"
+        c.Search = "economy OR recession OR stock OR inflation OR economic growth"
         c.Limit = 50
         c.Store_csv = True
         c.Output = "tweets.csv"
