6b285c6caeea371fe94468a2d8d864fd61745d57
Rosan Wang <rosanwang@yahoo.com>
2023-04-09 21:04:52 -0400

    got all the tweets~

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import tweepy
+import csv
 
 auth = tweepy.OAuth1UserHandler(
     "ueRXxydM3Vg3ppdbdKsdptjnL", "hhwbCTPKD3uAy6ikUBNsWhCWQ7lXPxUXjCRknysgkCpP9CUo8f"
@@ -6,5 +7,22 @@ auth = tweepy.OAuth1UserHandler(
 api = tweepy.API(auth)
 search_results = api.search_tweets("economy")
 
-for i in search_results:
-    print(i)
+# Open/create a file to append data to
+csvFile = open('result.csv', 'a')
+
+#Use csv writer
+csvWriter = csv.writer(csvFile)
+#print(search_results)
+
+for tweet in search_results:
+    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
+    print(tweet.created_at, tweet.text)
+
+csvFile.close()
+# Write a row to the CSV file. I use encode UTF-8
+
+    #print(i)
+
+#curl "https://api.twitter.com/1.1/tweets/search/fullarchive/test/counts.json?query=TwitterDev%20%5C%22search%20api%5C%22" -H "Authorization: Bearer AAAAAAAAAAAAAAAAAAAAAIbkmAEAAAAAPqZjYeHTfgNyJM0b0%2BdEwWKZgI4%3DE9bngHR8cyR6lUsOf98ivqfy9T2JNVxY1g0F3U9BNIXKLMvSJB"
+#curl https://api.twitter.com/2/tweets/search/recent?query=cat%20has%3Amedia%20-grumpy&max_results=100 -H "Authorization: Bearer $BEARER_TOKEN"
+#curl "https://api.twitter.com/2/tweets/search/fullarchive/test/counts.json?query=cat&max_results=100>" -H "Authorization: Bearer AAAAAAAAAAAAAAAAAAAAAIbkmAEAAAAAPqZjYeHTfgNyJM0b0%2BdEwWKZgI4%3DE9bngHR8cyR6lUsOf98ivqfy9T2JNVxY1g0F3U9BNIXKLMvSJB"
---
+++ tweets.py
@@ -0,0 +1,22 @@
+import twint
+import datetime
+
+# for some reason only tweets from 2007-03-01 onwards exist
+for year in range(2007, 2022):
+    for month in range(1, 12):
+        since = datetime.datetime(year, month, 1)
+        until = datetime.datetime(year, month + 1, 1)
+
+        c = twint.Config()
+        c.Search = "economy"
+        c.Limit = 50
+        c.Store_csv = True
+        c.Output = "tweets.csv"
+        c.Since = str(since)
+        c.Hide_output = True
+        c.Until = str(until)
+        print(since)
+        print(until)
+        twint.run.Search(c)
+
+
