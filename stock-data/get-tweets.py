b4b492831039406bf4492ae88b26eb61383f4d3d
eco20 <eireanco@gmail.com>
2023-04-20 02:33:42 +0800

    update get tweets

--- stock-data/get-tweets.py
+++ stock-data/get-tweets.py
@@ -2,7 +2,7 @@ import twint
 import datetime
 
 # for some reason only tweets from 2007-03-01 onwards exist
-for year in range(2008, 2023):
+for year in range(2008, 2022):
     for month in range(1, 12):
         since = datetime.datetime(year, month, 1)
         until = datetime.datetime(year, month + 1, 1)
@@ -10,7 +10,7 @@ for year in range(2008, 2023):
         c = twint.Config()
         c.Search = "economy OR recession OR stock OR inflation OR economic growth OR employment"
         c.Min_likes = 100
-        c.Limit = 100
+        c.Limit = 500
         c.Store_csv = True
         c.Output = "tweets.csv"
         c.Since = str(since)
